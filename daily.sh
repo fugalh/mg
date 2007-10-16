a7ef73069b544cbb4cf0cf0d4508646ccb0b5d49
hans <hans@fugal.net>
2007-10-16 13:49:45 -0600

    daily
    darcs-hash:20071016194945-28bda-d6192250e7bc509c9528b732c74bd3375301fe48.gz

---
+++ daily.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+mg=$HOME/src/mg
+dat=$HOME/.mg
+svg=$HOME/public_html/images/mg.svg
+png=$HOME/public_html/images/mg.png
+
+if [ $# -gt 0 ]; then
+    sh $mg/mg.sh "$@" >> $dat
+fi
+ruby $mg/mg.rb <$dat >$svg
