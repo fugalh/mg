3fe92017cf4a133c1c6bb9c093eb784c6a0b47b1
hans <hans@fugal.net>
2007-10-16 13:56:30 -0600

    daily readme
    darcs-hash:20071016195630-28bda-edab70fea616a0f253444dc48acd466623b6e7be.gz

--- daily.sh
+++ daily.sh
@@ -5,6 +5,7 @@ svg=$HOME/public_html/images/mg.svg
 png=$HOME/public_html/images/mg.png
 
 if [ $# -gt 0 ]; then
+    sh $mg/mg.sh "$@" # print it out on the console
     sh $mg/mg.sh "$@" >> $dat
 fi
 ruby $mg/mg.rb <$dat >$svg
